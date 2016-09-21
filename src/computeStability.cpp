13a2417c5801be4d0240d8af726c3f434624cfac
sunthedeep <sandeep1988@gmail.com>
2016-09-21 15:10:27 -0400

    Update computeStability.cpp

--- src/computeStability.cpp
+++ src/computeStability.cpp
@@ -6,6 +6,17 @@
 #include <gsl/gsl_linalg.h>
 #include "stability.h"
 using namespace std;
+
+/*
+	Computes whether a set of alleles can be maintained as a stable polymorphism according to the method of Kimura et al 1969
+	Command line arguments for input: 
+	d - the number of alleles to be tested, an integer value
+	a further (d*(d+1)/2) arguments defining the fitness of all possible genotypes for the d alleles. 
+	
+	the order of these fitness value arguments should essentially be a row-major representation of the upper-right half of the dxd fitness matrx
+	e.g. if there are 3 alleles A, B and C, the fitness values should be given in the order of AA, AB, AC, BB, BC, CC
+*/
+
 int main(int argc, char* argv[]){
 	if(argc < 3){
 		cerr<<"Insufficient arguments!";
